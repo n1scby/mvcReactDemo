e3309b15fab81f9b1cb113b82c4c2fb9f52761e3
Nic B <n1scby@yahoo.com>
2018-08-21 09:34:35 -0400

    initial commit - api

---
+++ ReactCrudDemo/ReactCrudDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ReactCrudDemo.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ReactCrudDemo.Controllers
+{
+   
+    public class EmployeeController : Controller
+    {
+
+        EmployeeDataAccessLayer objEmployee = new EmployeeDataAccessLayer();
+
+        // GET: api/<controller>
+        [HttpGet]
+        [Route("api/Employee/Index")]
+        public IEnumerable<TblEmployee> Index()
+        {
+            return objEmployee.GetAllEmployees();
+        }
+
+        // GET Details
+        [HttpGet]
+        [Route("api/Employee/Details/{id}")]
+        public TblEmployee Details(int id)
+        {
+            return objEmployee.GetEmployeeData(id);
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        [Route("api/Employee/Create")]
+        public int Create(TblEmployee employee)
+        {
+            return objEmployee.AddEmployee(employee);
+        }
+
+        // PUT 
+        [HttpPut]
+        [Route("api/Employee/Edit")]
+        public int Edit(TblEmployee employee)
+        {
+            return objEmployee.UpdateEmployee(employee);
+        }
+
+        // DELETE 
+        [HttpDelete]
+        [Route("api/Employee/Delete/{id}")]
+        public int Delete(int id)
+        {
+            return objEmployee.DeleteEmployee(id);
+        }
+
+        [HttpGet]
+        [Route ("api/Employee/GetCityList")]
+        public IEnumerable<TblCities> Details()
+        {
+            return objEmployee.GetCities();
+        }
+
+    }
+}
---
+++ ReactCrudDemo/ReactCrudDemo/Models/EmployeeDataAccessLayer.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace ReactCrudDemo.Models
+{
+    public class EmployeeDataAccessLayer
+    {
+        ReactCrudDemoContext db = new ReactCrudDemoContext();
+
+        // to get list of all employees
+        public IEnumerable<TblEmployee> GetAllEmployees()
+        {
+            try
+            {
+                return db.TblEmployee.ToList();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        //To add new employee record
+        public int AddEmployee(TblEmployee employee)
+        {
+            try
+            {
+                db.TblEmployee.Add(employee);
+                db.SaveChanges();
+                return 1;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        //To update the record of an employee
+        public int UpdateEmployee(TblEmployee employee)
+        {
+            try
+            {
+                db.Entry(employee).State = EntityState.Modified;
+                db.SaveChanges();
+
+                return 1;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        //Get Employee details
+        public TblEmployee GetEmployeeData(int id)
+        {
+            try
+            {
+                TblEmployee employee = db.TblEmployee.Find(id);
+                return employee;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        //Delete Employee
+        public int DeleteEmployee(int id)
+        {
+            try
+            {
+                TblEmployee emp = db.TblEmployee.Find(id);
+                db.TblEmployee.Remove(emp);
+                db.SaveChanges();
+                return 1;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        //Get list of cities
+        public List<TblCities> GetCities()
+        {
+            List<TblCities> lstCity = new List<TblCities>();
+            lstCity = (from CityList in db.TblCities select CityList).ToList();
+            return lstCity;
+        }
+
+
+    }
+}
